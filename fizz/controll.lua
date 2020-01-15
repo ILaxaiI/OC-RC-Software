a2e449d8032b6b6965c92f917d910a6fe9c430d0
ILaxaiI <58485516+ILaxaiI@users.noreply.github.com>
2020-01-16 00:24:18 +0100

    Update controll.lua

--- fizz/controll.lua
+++ fizz/controll.lua
@@ -1,1 +1,27 @@
-print("hello world")
+local reactor = require("component").nc_fission_reactor
+local event = require("event")
+
+
+
+local function eventHandler(eventId,...)
+  print(eventId,...)
+  reactor.deactivate()
+  os.exit()
+end
+
+event.listen("key_down",eventHandler)
+
+while true do
+  local heat,maxHeat = reactor.getHeatLevel(),reactor.getMaxHeatLevel()
+  local energy,maxEnergy = reactor.getEnergyStored(),reactor.getMaxEnergyStored()
+  print("Heat: "..heat.."/"..maxHeat.."\nRF: "..energy.."/"..maxEnergy)
+  if energy >= maxEnergy*0.99 or heat >= maxHeat/2 then
+    reactor.deactivate()
+  else
+    reactor.activate()
+  end
+end
+
+
+
+
