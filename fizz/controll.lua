f3ce19a30e2a32e7fe36a89092c977bd168a7848
ILaxaiI <58485516+ILaxaiI@users.noreply.github.com>
2020-01-16 00:30:49 +0100

    Update controll.lua

--- fizz/controll.lua
+++ fizz/controll.lua
@@ -1,20 +1,23 @@
-local reactor = require("component").nc_fission_reactor
+local comps = require("component")
+local reactor = comps.nc_fission_reactor
+local gpu = comps.gpu
 local event = require("event")
-
 
 
 local function eventHandler(eventId,...)
   print(eventId,...)
   reactor.deactivate()
-  os.exit()
 end
 
 event.listen("key_down",eventHandler)
 
 while true do
+  os.execute("cls")
+  print(event.pull(0.1))
   local heat,maxHeat = reactor.getHeatLevel(),reactor.getMaxHeatLevel()
   local energy,maxEnergy = reactor.getEnergyStored(),reactor.getMaxEnergyStored()
-  print("Heat: "..heat.."/"..maxHeat.."\nRF: "..energy.."/"..maxEnergy)
+  gpu.set(10,10,"Heat: "..heat.."/"..maxHeat)
+  gpu.set(10,12,"RF: "..energy.."/"..maxEnergy)
   if energy >= maxEnergy*0.99 or heat >= maxHeat/2 then
     reactor.deactivate()
   else
