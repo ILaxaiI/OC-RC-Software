e2719e71e13c9af9629b7497690299469ce2b957
ILaxaiI <58485516+ILaxaiI@users.noreply.github.com>
2020-01-16 00:33:55 +0100

    Update controll.lua

--- fizz/controll.lua
+++ fizz/controll.lua
@@ -12,19 +12,20 @@ end
 event.listen("key_down",eventHandler)
 
 while true do
-  os.execute("cls")
-  print(event.pull(0.1))
   local heat,maxHeat = reactor.getHeatLevel(),reactor.getMaxHeatLevel()
   local energy,maxEnergy = reactor.getEnergyStored(),reactor.getMaxEnergyStored()
-  gpu.set(10,10,"Heat: "..heat.."/"..maxHeat)
-  gpu.set(10,12,"RF: "..energy.."/"..maxEnergy)
+  
   if energy >= maxEnergy*0.99 or heat >= maxHeat/2 then
     reactor.deactivate()
   else
     reactor.activate()
   end
+  
+--  os.execute("cls")
+  gpu.set(10,10,"Heat: "..heat.."/"..maxHeat)
+  gpu.set(10,12,"RF: "..energy.."/"..maxEnergy)
+  
+
 end
-
-
 
 
